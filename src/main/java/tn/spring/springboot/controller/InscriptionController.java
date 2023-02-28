78829442fac4aa4a3dbee78f9d4c43d00d5b819c
raniahadhri <rania.hadhri@esprit.tn>
2023-02-28 12:27:54 +0100

    Create CRUD of all entities

--- src/main/java/tn/spring/springboot/controller/InscriptionController.java
+++ src/main/java/tn/spring/springboot/controller/InscriptionController.java
@@ -21,6 +21,7 @@ public class InscriptionController {
     @GetMapping("/{numInscription}")
     public Inscription retrieveInscription(@PathVariable Long numInscription) {
         return InscriptionService.retrieveInscription(numInscription);
+
     }
 
     @PostMapping
